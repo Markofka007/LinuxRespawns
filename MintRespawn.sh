1cf512d72934f6f3247739626fb992e2848ec33c
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2023-02-03 10:58:48 -0700

    Fixed a few issues with `sudo apt install`
    This still doesn't work, just fixing a few issues

--- MintRespawn.sh
+++ MintRespawn.sh
@@ -2,14 +2,14 @@
 
 # Move to home dir and make sure git and pip are installed
 cd ~
-sudo apt install python3-pip -y
-sudo apt install git -y
+sudo apt install -y python3-pip
+sudo apt install -y git
 
 # Sublime Text install
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install sublime-text
+sudo apt install -y sublime-text
 
 # Make a tools dir and download linpeas
 mkdir Tools && cd Tools
@@ -24,7 +24,7 @@ gunzip rockyou.txt.gz
 wget https://github.com/daviddias/node-dirbuster/raw/master/lists/directory-list-2.3-medium.txt
 
 # Install gobuster
-sudo apt install gobuster
+sudo apt install -y gobuster
 
 # Install metasploit
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
